8f0c34dd4c20f3e3d060b0eedb921731ce6035c6
Raul Quispe <raul.quispe@live.com>
2018-08-09 18:54:30 -0500

    login

--- App/Source/Views/LogIn/LogInView.swift
+++ App/Source/Views/LogIn/LogInView.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import RxSwift
+import RxCocoa
 class LogInView: UIViewController {
     
     override func viewDidLoad() {
@@ -15,6 +16,7 @@ class LogInView: UIViewController {
 
         // Do any additional setup after loading the view.
         self.addValues()
+        self.binding()
     }
     
     override func didReceiveMemoryWarning() {
@@ -34,12 +36,48 @@ class LogInView: UIViewController {
         self.btnEnter.layer.borderColor = UIColor.black.cgColor
         
     }
+    let disposeBag = DisposeBag()
+    func binding() {
+        txtfUsername.rx.text.asDriver().drive(txtfPassword.rx.text)
+            .disposed(by: disposeBag)
+    }
+  
     // MARK: - UI Elements
   
     @IBOutlet weak var txtfUsername: UITextField!
     @IBOutlet weak var txtfPassword: UITextField!
     
     @IBOutlet weak var btnEnter: UIButton!
+
+    @IBAction func getProfile(_ sender: Any) {
+        let username = "aquima"
+        let password = "olokb7i88"
+        let loginString = String(format: "%@:%@", username, password)
+        let loginData = loginString.data(using: String.Encoding.utf8)!
+        let base64LoginString = loginData.base64EncodedString()
+        
+        // create the request
+        let url = URL(string: "https://api.github.com/user")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+
+        let session: URLSession
+        session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: request, completionHandler: { data, response, error in
+            guard let data = data else { return }
+            do {
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options:
+                    JSONSerialization.ReadingOptions.mutableContainers)
+                print(jsonResult)
+
+            } catch let err {
+
+            }
+            
+        })
+        task.resume()
+    }
     /*
     // MARK: - Navigation
 
